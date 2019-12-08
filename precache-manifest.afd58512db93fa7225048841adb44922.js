502e7032c471d174a90a2ffc1c79e9f6db74b066
Doğukan Arslan <dogukan2arslan@hotmail.com>
2019-12-09 00:12:03 +0300

    Updates

---
+++ precache-manifest.afd58512db93fa7225048841adb44922.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f841575fb570022301e35c07726c4b8",
+    "url": "/pets-application/index.html"
+  },
+  {
+    "revision": "d78b991876d5f5a449b6",
+    "url": "/pets-application/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "66627640e14dc7d849c6",
+    "url": "/pets-application/static/css/main.7bf197c9.chunk.css"
+  },
+  {
+    "revision": "d78b991876d5f5a449b6",
+    "url": "/pets-application/static/js/2.d0933681.chunk.js"
+  },
+  {
+    "revision": "66627640e14dc7d849c6",
+    "url": "/pets-application/static/js/main.07f0c81b.chunk.js"
+  },
+  {
+    "revision": "afd6ebc0bb716b789963",
+    "url": "/pets-application/static/js/runtime-main.018e0dee.js"
+  }
+]);
